d18609144df673c8b3ddb1a8f918a15b0720ad2b
Roman <myshchyshyn_roman@ukr.net>
2025-06-10 01:00:44 +0300

    small useEffect fix

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ProfilePage.module.css";
 import Header from "../../components/Header/Header";
 import {Link, useNavigate, useNavigation} from "react-router-dom";
