b8d70ed57bfba9bbcc8611838126ef99e1b1c76b
RomanLazor <sasredemption123@gmail.com>
2025-06-09 16:37:31 +0300

    Fixing Map Page

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import GreekCategory from './pages/Categories/GreekCategory/GreekCategory';
 import JapaneseCategory from './pages/Categories/JapaneseCategory/JapaneseCategory';
 import SpanishCategory from './pages/Categories/SpanishCategory/SpanishCategory';
 import CreateNewPassword from './pages/CreateNewPassword/CreateNewPassword';
+import MapTest from './pages/Map/MapTest';
 
 
 
@@ -46,12 +47,15 @@ function App() {
             <Route path="/categories/japanese" element={<JapaneseCategory />} />
             <Route path="/categories/spanish" element={<SpanishCategory />} />
             <Route path="/login" element={<LogIn />} />
-            <Route path="/map" element={
+            <Route path="/map" element={<UkraineMap />} />
+
+            
+           {/* <Route path="/map" element={
                 <PrivateRoute>
                   <UkraineMap />
                 </PrivateRoute>
               }
-            />
+            />*/}
             <Route path="/profile" element=
               {
                 <PrivateRoute>
